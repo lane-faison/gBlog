8395733f2cfabe238afcfb92d3382813b69359b0
lanefaison2 <faisonusmc@gmail.com>
2017-03-06 16:06:24 -0700

    Heroku points to html

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 8000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use('/blogs', gblogs)
+app.use(express.static("./public"))
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
