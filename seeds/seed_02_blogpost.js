6d586630ad2caea49879047f236c2ef1314b3c16
lanefaison2 <faisonusmc@gmail.com>
2017-03-06 19:09:56 -0700

    css

--- seeds/seed_02_blogpost.js
+++ seeds/seed_02_blogpost.js
@@ -6,7 +6,14 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('blogpost').insert([
         {author_id: 1, title: 'title1', body: 'something1 something1 something1'},
-        {author_id: 2, title: 'title2', body: 'something2 something2 something2'}
+        {author_id: 2, title: 'title2', body: 'something2 something2 something2'},
+        {author_id: 3, title: 'title2', body: 'something2 something2 something2'},
+        {author_id: 1, title: 'title2', body: 'something2 something2 something2'},
+        {author_id: 2, title: 'title2', body: 'something2 something2 something2'},
+        {author_id: 3, title: 'title2', body: 'something2 something2 something2'},
+        {author_id: 1, title: 'title2', body: 'something2 something2 something2'},
+        {author_id: 2, title: 'title2', body: 'something2 something2 something2'},
+        {author_id: 3, title: 'title2', body: 'something2 something2 something2'}
       ])
     })
 }
