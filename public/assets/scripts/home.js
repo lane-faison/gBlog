388ed017e815cbee0017085ce68d228d984de6c6
lanefaison2 <faisonusmc@gmail.com>
2017-03-07 09:14:58 -0700

    css

--- public/assets/scripts/home.js
+++ public/assets/scripts/home.js
@@ -11,9 +11,7 @@ $(document).ready(function () {
       newId = data.length
 
       $('.blog-space').append(
-        `<div class="blog-square col-sm-6 col-md-4"><div class="thumbnail" id=${count}><img class="blog-image" src="${data[i].image}"></div></div>`
-
-        // <h3 class="blog-title">${data[i].title}</h3><p class="blog-body">${data[i].body}</p></div>
+        `<div class="blog-square col-sm-6 col-md-4"><div class="thumbnail" id=${count}><img class="blog-image" src="${data[i].image}"></div></div><h3 class="blog-title">${data[i].title}</h3></div>`
       )
     }
   })
