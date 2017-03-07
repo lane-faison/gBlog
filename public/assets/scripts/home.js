d67e0d4095790b4394a5a66ed96276dd1c1a0f54
lanefaison2 <faisonusmc@gmail.com>
2017-03-07 10:08:33 -0700

    css

--- public/assets/scripts/home.js
+++ public/assets/scripts/home.js
@@ -11,11 +11,13 @@ $(document).ready(function () {
       newId = data.length
 
       $('.blog-space').append(
-        `<div class="blog-square col-sm-6 col-md-4"><div class="thumbnail" id="dammit"><img class="blog-image" src="${data[i].image}"><h3 class="blog-title">${data[i].title}</h3></div></div>`
+        `<div class="blog-square col-sm-6 col-md-4"><div class="thumbnail" id="dammit"><img class="blog-image" src="${data[i].image}"><h3 class="blog-title">${data[i].title}</h3><p class="blog-body">${data[i].body}</p></div></div>`
       )
-      $(document).on('mouseover','#dammit', function () {
-        $('#dammit').append(`<p class="blog-body">${data[i].body}</p>`)
-      })
     }
   })
+})
+
+$(document).on('mouseover','#dammit', function () {
+  $('#dammit.p').show()
+  $('#dammit.h3').hide()
 })
