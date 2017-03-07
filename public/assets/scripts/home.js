cb4b3e9525e55cefda2a274ede1cf5033d6b8d0b
lanefaison2 <faisonusmc@gmail.com>
2017-03-07 10:14:40 -0700

    css

--- public/assets/scripts/home.js
+++ public/assets/scripts/home.js
@@ -17,10 +17,7 @@ $(document).ready(function () {
   })
 })
 
-$(document).on('mouseover','#dammit', function () {
+$(document).on('mouseover mouseleave','#dammit', function () {
   $('#dammit .blog-body').show()
   $('#dammit .blog-title').hide()
-}, function () {
-  $('#dammit .blog-body').hide()
-  $('#dammit .blog-title').show()
 })
