b096e69a8bfb2386625ebc5b8ec9d814ba0f6052
lanefaison2 <faisonusmc@gmail.com>
2017-03-07 16:32:10 -0700

    update

--- migrations/20170306105051_create_author.js
+++ migrations/20170306105051_create_author.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('author', table => {
     table.increments()
     table.string('email').unique()
-    table.string('name').unique()
+    table.string('name')
   })
 }
 
