8413fd6cd2a5d11cf5a2e42d8517227721f72afd
lanefaison2 <faisonusmc@gmail.com>
2017-03-07 11:40:20 -0700

    appending body work

--- migrations/20170306112656_create_blogpost.js
+++ migrations/20170306112656_create_blogpost.js
@@ -3,7 +3,6 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('blogpost', table => {
     table.increments()
     table.integer('author_id').references('author.id')
-    table.string('author_name').references('author.name')
     table.string('title')
     table.text('body')
     table.string('image')
